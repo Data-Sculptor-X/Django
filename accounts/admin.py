bbf7b5381f0f58698de444c3a52eab1a35a55dec
santhoshparthiban <santhoshparthiban2002@gmail.com>
2024-01-13 21:12:47 +0530

    changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display= ('username','name','email','phone_no','active','tfa')
+
+class UserTrackAdmin(admin.ModelAdmin):
+	list_display= ('username','refresh_token','count','remember_me','login','logout')
+
+admin.site.register(UserProfile,UserProfileAdmin)
+admin.site.register(UserTrack,UserTrackAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 # # Create your models here.
 
 
-# class UserProfile(models.Model):
-# 	username = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True) 
-# 	name = models.CharField(max_length=255)  
-# 	dob = models.CharField(max_length=50, null=True,blank=True)
-# 	email = models.DateTimeField(auto_now_add=True)
-# 	phone_no = models.DateTimeField(null=True, blank=True) 
-# 	profile_picture = models.JSONField( null=True, blank=True) 
-# 	secret_key = models.JSONField( null=True, blank=True) 
-# 	locked = models.JSONField( null=True, blank=True) 
-# 	active = models.JSONField( null=True, blank=True) 
+class UserProfile(models.Model):
+	username = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True) 
+	name = models.CharField(max_length=255, null=True,blank=True)  
+	dob = models.DateField( null=True,blank=True)
+	email = models.CharField(max_length=255, null=True,blank=True) 
+	phone_no = models.CharField(max_length=255, null=True,blank=True) 
+	profile_picture = models.ImageField( upload_to='userProfile/',null=True, blank=True) 
+	secret_key = models.CharField(max_length=255, null=True,blank=True) 
+	locked = models.CharField(max_length=255, null=True,blank=True) 
+	active = models.BooleanField( null=True, blank=True) 
+	tfa = models.BooleanField( null=True, blank=True)
 	
 # class OrganizationProfile(models.Model):
 # 	username = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True) 
@@ -24,10 +25,11 @@ from django.db import models
 # 	locked = models.JSONField( null=True, blank=True) 
 # 	active = models.JSONField( null=True, blank=True) 
 	
-# class UserTrack(models.Model):
-# 	username = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True) 
-# 	refresh_token = models.CharField(max_length=255)  
-# 	status = models.CharField(max_length=50, null=True,blank=True)
-# 	login = models.DateTimeField(auto_now_add=True)
-# 	logout = models.DateTimeField(null=True, blank=True) 
-# 	user_details = models.JSONField( null=True, blank=True) 
+class UserTrack(models.Model):
+	username = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True) 
+	refresh_token = models.CharField(max_length=255,null=True, blank=True)  
+	login = models.DateTimeField(auto_now_add=True)
+	logout = models.DateTimeField(null=True, blank=True) 
+	remember_me= models.BooleanField(null=True, blank=True) 
+	count = models.CharField(max_length=255,null=True, blank=True) 
+	user_details = models.JSONField( null=True, blank=True) 
